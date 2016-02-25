da62e3f29fad3c90cbe53b3f0eedc685829d9190
이창민 <airkiss81@gmail.com>
2016-02-25 20:55:07 +0900

    32424

--- Simulater/Form1.Designer.cs
+++ Simulater/Form1.Designer.cs
@@ -31,9 +31,16 @@
             this.dgSchedule = new System.Windows.Forms.DataGridView();
             this.dgMatch = new System.Windows.Forms.DataGridView();
             this.dgPitcher = new System.Windows.Forms.DataGridView();
+            this.dgBatter = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgBat = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgSchedule)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgMatch)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgPitcher)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBatter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBat)).BeginInit();
             this.SuspendLayout();
             // 
             // dgSchedule
@@ -74,11 +81,64 @@
             this.dgPitcher.Size = new System.Drawing.Size(164, 82);
             this.dgPitcher.TabIndex = 4;
             // 
+            // dgBatter
+            // 
+            this.dgBatter.AllowUserToAddRows = false;
+            this.dgBatter.AllowUserToDeleteRows = false;
+            this.dgBatter.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBatter.Location = new System.Drawing.Point(352, 12);
+            this.dgBatter.Name = "dgBatter";
+            this.dgBatter.ReadOnly = true;
+            this.dgBatter.RowTemplate.Height = 23;
+            this.dgBatter.Size = new System.Drawing.Size(123, 491);
+            this.dgBatter.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(815, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(815, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(815, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgBat
+            // 
+            this.dgBat.AllowUserToAddRows = false;
+            this.dgBat.AllowUserToDeleteRows = false;
+            this.dgBat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBat.Location = new System.Drawing.Point(481, 95);
+            this.dgBat.Name = "dgBat";
+            this.dgBat.ReadOnly = true;
+            this.dgBat.RowTemplate.Height = 23;
+            this.dgBat.Size = new System.Drawing.Size(434, 408);
+            this.dgBat.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(927, 515);
+            this.Controls.Add(this.dgBat);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgBatter);
             this.Controls.Add(this.dgPitcher);
             this.Controls.Add(this.dgMatch);
             this.Controls.Add(this.dgSchedule);
@@ -88,7 +148,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgSchedule)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgMatch)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgPitcher)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBatter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBat)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,6 +160,11 @@
         private System.Windows.Forms.DataGridView dgSchedule;
         private System.Windows.Forms.DataGridView dgMatch;
         private System.Windows.Forms.DataGridView dgPitcher;
+        private System.Windows.Forms.DataGridView dgBatter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgBat;
     }
 }
 
--- Simulater/Form1.cs
+++ Simulater/Form1.cs
@@ -20,6 +20,8 @@ namespace Simulater
         {
             InitializeComponent();
             dgSchedule.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            textBox1.Text = "74339";
+            textBox2.Text = "77637";
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -53,6 +55,7 @@ namespace Simulater
 
         private void dgMatch_SelectionChanged(object sender, EventArgs e)
         {
+            // 투수
             String gameId = dgMatch.CurrentRow.Cells[0].Value.ToString();
 
             var matchs = dbMgr.SelectAll<Match>();
@@ -80,8 +83,90 @@ namespace Simulater
 
             
             dgPitcher.DataSource = queryBat;
+
+            // 타자
+            var queryThBatter = from m in matchs
+                          join s in schedules
+                          on m.GameId equals s.GameId
+                          join t in ths
+                          on m.Id equals t.MatchId
+                          where m.GameId == gameId 
+                          select t;
+
+            var batBatter = from t in queryThBatter
+                            join b in bats
+                           on t.Id equals b.ThId
+                            join p in players
+                            on b.HitterId equals p.PlayerId
+                            select new
+                            {
+                                PlayerId = b.HitterId,
+                                Name = p.Name,
+                                PResult = b.PResult
+                            };
+
+            var query = from b in batBatter
+                        group b by b.PlayerId into g
+                        select new
+                        {
+                            Name = g.Max(x => x.Name),
+                            Result = g.Sum(x => x.PResult == PResultType.Hit ? 1 : 0)
+                        };
+
+            dgBatter.DataSource = query;
         }
 
 
+        private Double CalcRatio(Int32 batterId, Int32 pitcherId)
+        {
+            // 1 투수를가져온다.
+            var players = dbMgr.SelectAll<Player>();
+            var pitcher = (from p in players
+                           where p.PlayerId == pitcherId
+                           select p).First();
+
+            // 타자를 가져온다.
+            var batter = (from p in players
+                          where p.PlayerId == batterId
+                          select p).First();
+
+            var matchs = dbMgr.SelectAll<Match>();
+            var schedules = dbMgr.SelectAll<Schedule>();
+            var ths = dbMgr.SelectAll<Th>();
+            var bats = dbMgr.SelectAll<Bat>();
+
+            var pitcherBats = from m in matchs
+                              join s in schedules
+                              on m.GameId equals s.GameId
+                              join t in ths
+                              on m.Id equals t.MatchId
+                              join b in bats
+                              on t.Id equals b.ThId
+                              join ph in players
+                              on b.HitterId equals ph.PlayerId
+                              where b.PitcherId == pitcher.PlayerId
+                                   && ph.Hand.Substring(2, 2) == batter.Hand.Substring(2, 2) 
+                               select b;
+
+            var pitcherBatsG = from p in pitcherBats
+                               group p by p.PitcherId into g
+                               select new
+                               {
+                                   HitCount = g.Sum(x => x.PResult == PResultType.Hit ? 1 : 0),
+                                   TotalCount = g.Sum(x => 1),
+                                   Ratio = Math.Round(g.Sum(x => x.PResult == PResultType.Hit ? 1 : 0) * 1.0m 
+                                   / g.Sum(x => 1), 3, MidpointRounding.AwayFromZero)
+                               };
+
+
+            dgBat.DataSource = pitcherBats;
+
+            return 0.0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ratio = CalcRatio(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
     }
 }
